aa7fca351b979bdee11c258796168728ed2bfbc6
u1710115 <n.murodova@student.inha.uz>
2019-05-11 10:42:46 +0500

    last lab done

---
+++ src/index.js
@@ -0,0 +1,6 @@
+//import 'bootstrap';
+//import './scss/styles.scss'
+
+$(document).ready(function(){
+	$('.header').height($(window).height());
+})
---
+++ src/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const config = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(scss)$/,
+				use: [
+				  {
+				  	loader: 'style-loader'
+				  },
+				  {
+				  	loader: 'css-loader'
+				  },
+				  {
+				  	loader: 'postcss-loader',
+				  	options: {
+				  		plugins: function(){
+				  			return [
+				  				require('autoprefixer')
+				  			];
+				  		}
+				  	}
+				  },
+				  {
+				  	loader: 'sass-loader'
+				  }
+				]
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.html$/,
+				use: 'html-loader'
+			},
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html"
+		})
+	]
+}
+
+module.exports = config;
